a628a4e37245788a85dbae2f92453dc40a925c1b
SeungYongShim <syshim@me.com>
2022-05-08 17:19:41 +0900

    .

--- src/Minesweeper.Tests/Tests.fs
+++ src/Minesweeper.Tests/Tests.fs
@@ -10,3 +10,14 @@ let ``Should number is 3`` () =
     let ret = MineItem.ToString sut
     Assert.Equal("3", ret)
 
+[<Fact>]
+let CoveredSpec() =
+    let sut = Covered Zero
+    let ret = MineItem.ToString sut
+    Assert.Equal(".", ret)
+
+[<Fact>]
+let AddSpec() =
+    let sut = Covered One
+    let ret = MineItem.add sut
+    Assert.Equal(Two, match ret with Covered x -> x)
--- src/Minesweeper/Library.fs
+++ src/Minesweeper/Library.fs
@@ -3,6 +3,7 @@ namespace Minesweeper
 type MineItem =
     | Covered of MineItem
     | Bomb
+    | Zero
     | One
     | Two
     | Three
@@ -16,4 +17,26 @@ module MineItem =
     let ToString v =
         match v with
         | Covered _ -> "."
+        | Bomb -> "*"
+        | Zero -> "0"
+        | One -> "1"
+        | Two -> "2"
         | Three -> "3"
+        | Four -> "4"
+        | Five -> "5"
+        | Six -> "6"
+        | Seven -> "7"
+        | Eight -> "8"
+    let rec add v =
+        match v with
+        | Covered x -> Covered (add x)
+        | Bomb -> Bomb
+        | Zero -> One
+        | One -> Two
+        | Two -> Three
+        | Three -> Four
+        | Four -> Five
+        | Five -> Six
+        | Six -> Seven
+        | Seven -> Eight
+
